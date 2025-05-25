76807475e1f24f2b201fbfa3ba5a874a12d5be2e
redruin1 <tfschaefer222@gmail.com>
2025-05-25 18:40:43 -0400

    I guess I learn some html now

--- doc/make.py
+++ doc/make.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         template_name = sys.argv[1]
     else:
-        template_name = "js_offline"
+        template_name = "js"
     main(template_name=template_name)
