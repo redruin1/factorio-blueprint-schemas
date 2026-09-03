e78051e9f80d1a385c1bbcfcb4746461d92cc22d
redruin1 <tfschaefer222@gmail.com>
2026-09-03 18:20:23 -0400

    adhere to DRY and bump versions

--- test/test_examples.py
+++ test/test_examples.py
@@ -23,19 +23,27 @@ test_reg = Registry(retrieve=retrieve_from_path)
 
 def gather_examples(path, extension):
     return [os.path.join(path, item) for item in os.listdir(path) if item.endswith(extension)]
-@pytest.mark.parametrize("filename", gather_examples(os.path.join("test", "examples"), ".bp"))
-def test_example_blueprints(filename):
+
+def do_test_schema(filename, version):
     # Decode the value to json
-    print(filename)
+        print(filename)
+    
+        with open(filename, "r") as file:
+            bp_string = file.read()
+    
+        json_dict = json.loads(zlib.decompress(base64.b64decode(bp_string[1:])))
+    
+        # We assume version 2.0
+        validator = Draft202012Validator(
+            grab_json_file("schemas", version, "blueprint.json"),
+            registry=test_reg
+        )
+        validator.validate(json_dict)
 
-    with open(filename, "r") as file:
-        bp_string = file.read()
+@pytest.mark.parametrize("filename", gather_examples(os.path.join("test", "examples", "2.0.0"), ".bp"))
+def test_example_blueprints_2_0(filename):
+    do_test_schema(filename, "2.0.0")
 
-    json_dict = json.loads(zlib.decompress(base64.b64decode(bp_string[1:])))
-
-    # We assume version 2.0
-    validator = Draft202012Validator(
-        grab_json_file("schemas", "2.0.0", "blueprint.json"),
-        registry=test_reg
-    )
-    validator.validate(json_dict)
+@pytest.mark.parametrize("filename", gather_examples(os.path.join("test", "examples", "2.1.0"), ".bp"))
+def test_example_blueprints_2_1(filename):
+    do_test_schema(filename, "2.1.0")
